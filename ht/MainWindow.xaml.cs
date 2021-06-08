315d695951e28aa217e7bfe2e18bb182504da1e9
jakubstellner <jakub.stellner03@gmail.com>
2021-06-08 16:59:23 +0200

    Edit, Update

--- ht/MainWindow.xaml.cs
+++ ht/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace ht
 
         public string _cs = @"Server=mssql.sps-prosek.local;Database=stellja18;User ID=stellja18;Password=stellja18";
         public int lastId = 0;
+        public int editId = 0;
 
         public MainWindow()
         {
@@ -45,70 +46,27 @@ namespace ht
 
             using var _cmd = new SqlCommand(_sql, _con);
 
-            /*string _ret = _cmd.ExecuteScalar().ToString();
-            Console.WriteLine(_ret);*/
-
             SqlDataReader _read = _cmd.ExecuteReader();
 
-
-            DataTable dt = new DataTable();
-
-            DataColumn name = new DataColumn("Jméno");
-            DataColumn surname = new DataColumn("Příjmení");
-            DataColumn city = new DataColumn("Přezdívka");
-
-
-            dt.Columns.Add(name);
-            dt.Columns.Add(surname);
-            dt.Columns.Add(city);
+            List<User> users = new List<User>();
 
             while (_read.Read())
             {
-                DataRow row = dt.NewRow();
-                row[0] = _read.GetString(1);
-                row[1] = _read.GetString(2);
-                row[2] = _read.GetString(3);
+                User user = new User();
 
-                dt.Rows.Add(row);
+                user.id = _read.GetInt32(0);
+                user.name = _read.GetString(1);
+                user.surname = _read.GetString(2);
+                user.username = _read.GetString(3);
 
-                this.lastId = _read.GetInt32(0)+1;
-
+                users.Add(user);
             }
 
-            myDataGrid.ItemsSource = dt.DefaultView;
-            
+            DG1.DataContext = users;
+
 
         }
 
-
-       /* void fillingData(List data)
-        {
-            DataTable dt = new DataTable();
-
-            DataColumn name = new DataColumn("Jméno");
-            DataColumn surname = new DataColumn("Příjmení");
-            DataColumn city = new DataColumn("Město");
-            DataColumn phone = new DataColumn("Mobil");
-
-
-            dt.Columns.Add(name);
-            dt.Columns.Add(surname);
-            dt.Columns.Add(city);
-            dt.Columns.Add(phone);
-
-            foreach (var item in data)
-            {
-                DataRow row = dt.NewRow();
-                row[0] = item.name;
-                row[1] = item.surname;
-                row[2] = item.city;
-                row[3] = item.phone;
-
-                dt.Rows.Add(row);
-            }
-
-            myDataGrid.ItemsSource = dt.DefaultView;
-        }*/
         private void addUser(object sender, RoutedEventArgs e)
         {
             string name_db = name.Text;
@@ -118,24 +76,104 @@ namespace ht
             name.Text = "";
             surname.Text = "";
             username.Text = "";
+            
 
 
             using var _con = new SqlConnection(_cs);
             _con.Open();
 
-            var _sql = "INSERT INTO [dbo].[user] ([id],[name], [surname],[username]) VALUES (@id,@name,@surname,@username)";
+            var type = ((Button)sender).Tag;
+            if ((string)type == "edit")
+            {
+                var _sql = "UPDATE [dbo].[user] SET [name] = @name, [surname] = @surname, [username] = @username WHERE [id] = @id";
 
+                using var _cmd = new SqlCommand(_sql, _con);
+                _cmd.Parameters.Add("@id", SqlDbType.Int).Value = editId;
+                _cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = name_db;
+                _cmd.Parameters.Add("@surname", SqlDbType.VarChar).Value = surname_db;
+                _cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = username_db;
+                var _ret = _cmd.ExecuteScalar();
+
+            }
+            else
+            {
+                var _sql = "INSERT INTO [dbo].[user] ([id],[name], [surname],[username]) VALUES (@id,@name,@surname,@username)";
+
+                using var _cmd = new SqlCommand(_sql, _con);
+                _cmd.Parameters.Add("@id", SqlDbType.Int).Value = lastId;
+                _cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = name_db;
+                _cmd.Parameters.Add("@surname", SqlDbType.VarChar).Value = surname_db;
+                _cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = username_db;
+
+                var _ret = _cmd.ExecuteScalar();
+            }
+            update.Content = "Přidat";
+            update.Tag = "insert";
+
+            updateData();
+
+        }
+
+        protected void EditButton_Click(object sender, EventArgs e)
+        {
+            var id = ((Button)sender).Tag;
+            this.editId = (int)id;
+
+            fillUpdateData((int)id);
+
+        }
+
+        protected void DeleteButton_Click(object sender, EventArgs e)
+        {
+            int id = (int)((Button)sender).Tag;
+
+            using var _con = new SqlConnection(_cs);
+            _con.Open();
+
+            var _sql = "DELETE FROM [dbo].[user] WHERE id = @id";
             using var _cmd = new SqlCommand(_sql, _con);
-            _cmd.Parameters.Add("@id", SqlDbType.Int).Value = lastId;
-            _cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = name_db;
-            _cmd.Parameters.Add("@surname", SqlDbType.VarChar).Value = surname_db;
-            _cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = username_db;
-
+            _cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
 
             var _ret = _cmd.ExecuteScalar();
 
-
             updateData();
+
+
+        }
+
+
+        protected void fillUpdateData(int id)
+        {
+            using var _con = new SqlConnection(_cs);
+            _con.Open();
+
+            var _sql = "SELECT * FROM [dbo].[user] WHERE [id] = @id";
+
+            using var _cmd = new SqlCommand(_sql, _con);
+            _cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+
+            SqlDataReader _read = _cmd.ExecuteReader();
+            
+
+            while (_read.Read())
+            {
+                name.Text = _read.GetString(1);
+                surname.Text = _read.GetString(2);
+                username.Text = _read.GetString(3);
+                update.Content = "Upravit";
+                update.Tag = "edit";
+            }
+            
+
+        
+        }
+
+
+        public class User {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string surname { get; set; }
+            public string username { get; set; }
 
         }
     }
